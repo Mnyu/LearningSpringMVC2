b7289e395b8907c6fee92474dac2a4a8be2d686e
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2021-10-25 16:43:05 +0530

    adding in-memory username password and bcrypt password encoder.

--- src/main/java/com/learnjava/frenemy/config/FrenemySpringSecurityConfig.java
+++ src/main/java/com/learnjava/frenemy/config/FrenemySpringSecurityConfig.java
@@ -1,12 +1,29 @@
 package com.learnjava.frenemy.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 // This class along with the @EnableWebSecurity helps us to create spring security filter chain
 
 @Configuration
 @EnableWebSecurity(debug = true)
 public class FrenemySpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1")
+                .password("$2a$10$EwGcPNYlvUdmgBx4qNjAxOinuU4GVKy3T93549lhZJQ220jbU6qk6")
+                .roles("admin");
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
